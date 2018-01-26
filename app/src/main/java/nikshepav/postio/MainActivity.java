ee2ec13cd7dd160fa4a35730b7ebbde1b79f62c6
NikshepAV <nikshep.av@gmail.com>
2018-01-26 21:36:54 +0530

    Final commit

--- app/src/main/java/nikshepav/postio/MainActivity.java
+++ app/src/main/java/nikshepav/postio/MainActivity.java
@@ -2,12 +2,36 @@ package nikshepav.postio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.web_view);
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://nikshepav-post-io.herokuapp.com");
+
     }
 }
